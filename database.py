f4852d2195f33d4295805255b14aa8a9ebcd2bcf
jomandac <web.bonum@gmail.com>
2026-09-15 13:36:11 +0200

    Update database.py

--- database.py
+++ database.py
@@ -6,6 +6,7 @@ DB_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'sportrez.db'
 def get_db():
     conn = sqlite3.connect(DB_PATH, timeout=30, check_same_thread=False)
     conn.row_factory = sqlite3.Row
+    conn.execute('PRAGMA foreign_keys = ON')
     return conn
 
 def init_db():
